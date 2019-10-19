5f9f29e6018b7af578c2d53ad2995aa5beaedc17
chauyiu1994 <choicy552019@gmail.com>
2019-10-20 00:11:22 +0800

    Fixed Broken Links. Close #45.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -16,10 +16,12 @@ public class VetController {
     }
 
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
     public String listVets(Model model) {
 
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
+
+
 }
